3b5482193f8e31cf2c3bc2286f502f992ff54f94
Amanda Ngo <amanda.ngo172@gmail.com>
2022-09-14 11:14:27 -0500

    Done with lab, no styling

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -19,7 +19,46 @@ router.get("/characters/:id", (req, res, next) => {
         res.render("characters/details-character", { character: responseFromAPI.data });
     })
     .catch(err => console.error(err))
-});
+}); 
+
+router.get('/characters/:id/edit', (req,res,next) => {
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        // console.log("details: ", responseFromAPI.data)
+        res.render("characters/edit-character", { 
+            character: responseFromAPI.data });
+    })
+    .catch(err => console.error(err))
+})
+
+
+router.post('/characters/:id/update', (req,res,next) => {
+    const charURL = `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`;
+
+    async function charEditor(){
+        if(req.body.name){
+            await axios.patch(charURL, {name: req.body.name});
+        }
+        if(req.body.occupation){
+            await axios.patch(charURL, {occupation: req.body.occupation});
+        }
+        if(req.body.weapon){
+            await axios.patch(charURL, {weapon: req.body.weapon});
+        }
+        res.redirect('/characters/'+ req.params.id)
+    }
+    charEditor();
+})
+
+
+router.post('/characters/:id/delete', (req,res,next) => {
+    const charURL = `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`;
+    axios.delete(charURL)
+        .then(() =>{
+            res.redirect('/characters')
+        })
+
+})
 
 module.exports = router;
 
